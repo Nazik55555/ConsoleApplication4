2e9fe9eb0d9c81d7c8ee8e33adaac1888368c1de
Nazik <Nazik@NAZIK>
2016-07-27 19:29:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string file = File.ReadAllText(@"e:/as.txt");
+        int[] nums = file
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(n => int.Parse(n))
+            .ToArray();
+        foreach (int i in nums)
+            Console.Write(i);
+    }
+}
